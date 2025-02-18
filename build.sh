c3f2a72f3bc38a4fbc95272db1d6686efa83f187
visual-art-bk <kekekj@naver.com>
2025-02-18 10:36:56 +0900

    빌드 방식 변경

--- build.sh
+++
@@ -1,6 +0,0 @@
-cd C:\Users\YourUsername\Projects\LightroomController  # 경로 변경
-pyinstaller --onedir --noupx --name="다비 촬영 매니저" `
-    --noconsole --noconfirm --clean `
-    --hidden-import=PySide6 `
-    --hidden-import=pywinauto `
-    src/main.py
